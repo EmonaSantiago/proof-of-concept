14c6a4697abac8f5cd890288b0be3ad092902585
Emona Santiago <emonasantiago1207@gmail.com>
2022-06-21 17:59:53 +0200

    code

--- main.js
+++ main.js
@@ -7,11 +7,13 @@ app.use(express.static('public'));
 
 app.set('view engome', 'ejs');
 app.set('views', 'public/views');
+app.set('view engine', 'ejs');
 
- app.get("/", (req, res) => {
+app.get("/", (req, res) => {
 
-   res.render('index');
+  res.render('index.ejs');
 
- });
+});
 
- app.listen(process.env.PORT || 3000, () => console.log("App avaiable on http://localhost:3000"))
+
+app.listen(process.env.PORT || 3000, () => console.log("App avaiable on http://localhost:3000"))
